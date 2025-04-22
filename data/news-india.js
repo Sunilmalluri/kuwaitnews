778ab9c38f3f8ac9c2bdbfe431b0b583b746dfba
Sunilmalluri <sunilmalluri08@gmail.com>
2025-04-22 07:40:40 +0000

    Update news-india.js

--- data/news-india.js
+++ data/news-india.js
@@ -1,1 +1,14 @@
-
+window.newsData = [
+    
+    {
+        "id": "ind1",
+        "title": "Welcome to India news",
+        "category": "India",
+        "date": "2025-04-21",
+        "time": "10:47 AM",
+        "image": "/images/news10.jpg",
+        "alt": "News",
+        "excerpt": "All the best...",
+        "fullText": "Good luck."
+    },
+  ];
