d5e3771953c3cc8fdf34d2f564ff07afada1f98d
Sunilmalluri <sunilmalluri08@gmail.com>
2025-04-28 12:06:58 +0300

    Create gold-price.js

---
+++ data/gold-price.js
@@ -0,0 +1,23 @@
+window.goldPriceData = [
+    {
+        "date": "2025-04-28",
+        "city": "Hyderabad",
+        "price_22k": "65000",
+        "price_24k": "71000",
+        "change": "+500"
+    },
+    {
+        "date": "2025-04-28",
+        "city": "Vijayawada",
+        "price_22k": "64800",
+        "price_24k": "70800",
+        "change": "+450"
+    },
+    {
+        "date": "2025-04-28",
+        "city": "Visakhapatnam",
+        "price_22k": "64900",
+        "price_24k": "70900",
+        "change": "-200"
+    }
+];
