ee3a641d4d54c1de9b99f533b60a2b263f7b6070
Sunilmalluri <sunilmalluri08@gmail.com>
2025-04-29 09:22:02 +0300

    Update gold-price-india.js

--- data/gold-price-india.js
+++ data/gold-price-india.js
@@ -4,47 +4,47 @@ window.goldPriceIndiaData = [
         "location": "Hyderabad",
         "currency": "INR",
         "currencySymbol": "₹",
-        "price_24k_1g": "7100",
-        "price_24k_10g": "71000",
-        "price_22k_1g": "6500",
-        "price_22k_10g": "65000",
-        "price_18k_1g": "5325",
-        "price_18k_10g": "53250"
+        "price_24k_1g": "9835",
+        "price_24k_10g": "98350",
+        "price_22k_1g": "9015",
+        "price_22k_10g": "90150",
+        "price_18k_1g": "7376",
+        "price_18k_10g": "73760"
     },
     {
         "date": "2025-04-28",
         "location": "Vijayawada",
         "currency": "INR",
         "currencySymbol": "₹",
-        "price_24k_1g": "7080",
-        "price_24k_10g": "70800",
-        "price_22k_1g": "6480",
-        "price_22k_10g": "64800",
-        "price_18k_1g": "5310",
-        "price_18k_10g": "53100"
+        "price_24k_1g": "9753",
+        "price_24k_10g": "97530",
+        "price_22k_1g": "8940",
+        "price_22k_10g": "89400",
+        "price_18k_1g": "7315",
+        "price_18k_10g": "73150"
     },
     {
         "date": "2025-04-28",
         "location": "Vizag",
         "currency": "INR",
         "currencySymbol": "₹",
-        "price_24k_1g": "7090",
-        "price_24k_10g": "70900",
-        "price_22k_1g": "6490",
-        "price_22k_10g": "64900",
-        "price_18k_1g": "5318",
-        "price_18k_10g": "53180"
+        "price_24k_1g": "9821",
+        "price_24k_10g": "98210",
+        "price_22k_1g": "9002",
+        "price_22k_10g": "90020",
+        "price_18k_1g": "7366",
+        "price_18k_10g": "73660"
     },
     {
         "date": "2025-04-28",
         "location": "Tirupati",
         "currency": "INR",
         "currencySymbol": "₹",
-        "price_24k_1g": "7070",
-        "price_24k_10g": "70700",
-        "price_22k_1g": "6470",
-        "price_22k_10g": "64700",
-        "price_18k_1g": "5303",
-        "price_18k_10g": "53030"
+        "price_24k_1g": "9821",
+        "price_24k_10g": "98210",
+        "price_22k_1g": "9002",
+        "price_22k_10g": "90020",
+        "price_18k_1g": "7366",
+        "price_18k_10g": "73660"
     }
 ];
